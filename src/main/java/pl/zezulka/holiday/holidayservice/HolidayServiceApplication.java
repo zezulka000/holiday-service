882b2c979399b1c434b167cf9a384197992f9ef3
zezulka000 <zezulka000@yahoo.com>
2020-10-11 22:37:35 +0200

    Init commit

---
+++ src/main/java/pl/zezulka/holiday/holidayservice/HolidayServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.zezulka.holiday.holidayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/zezulka/holiday/holidayservice/HolidayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zezulka.holiday.holidayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
